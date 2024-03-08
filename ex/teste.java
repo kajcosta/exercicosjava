d1405ebb93458d174b0677cd1665d31947542895
kajcosta <kk77665544jcmll@gmail.com>
2024-03-08 20:58:39 -0300

    Create teste.java

---
+++ ex/teste.java
@@ -0,0 +1,9 @@
+package ex;
+
+public class teste { 
+    public static void main(String[] args) {
+    int numeroInteiro = 32769;
+short numeroCurto = (short) numeroInteiro;
+System.out.println("Valor do short após a conversão: "+numeroCurto);
+}
+}
